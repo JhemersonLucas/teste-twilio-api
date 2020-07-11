ebb908659a621d2f10ace910a877a658950d58db
Jhemerson Lucas <jhemersonlucas@hotmail.com>
2020-07-11 12:24:20 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv/config');
+const twilio = require('twilio');
+
+const twilioClient = twilio(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+)
+
+// send message sms
+// twilioClient.messages.create({
+//     from: '+17732708038',
+//     to: '+5533999432780',
+//     body: 'Olá Jhemerson'
+// }).then(console.log).catch(console.error);
+
+// send message whatsapp
+twilioClient.messages.create({
+    from: 'whatsapp:+14155238886',
+    to: 'whatsapp:+553399432780',
+    body: 'Teste'
+}).then(console.log).catch(console.error);
+
+console.log(process.env.OWNER)
